0b8333f6f64731b4678b1fba0dbc952a8d0bd386
nindyashafira <nindyashafira24@gmail.com>
2023-10-12 16:52:46 +0700

    For Kelipatan Modifikasi

--- ForKelipatan22.java
+++ ForKelipatan22.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class ForKelipatan22 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int kelipatan, jumlah=0, counter=0;
+        int kelipatan, jumlah=0, counter=0, rata_rata;
 
         System.out.print("Masukkan bilangan kelipatan (1-9): ");
         kelipatan = scan.nextInt();
@@ -12,11 +12,11 @@ public class ForKelipatan22 {
             if (i % kelipatan == 0) {
                 jumlah += i;
                 counter ++;
-
             }
         }
-
+        rata_rata = jumlah / counter;
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, rata_rata);
     }
 }
